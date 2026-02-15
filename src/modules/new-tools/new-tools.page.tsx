9861894bf8f935e6d0ca99e106d5ca5ed2013a6c
naziachowdhurydev <naziachowdhurydev@gmail.com>
2026-02-15 21:08:11 +0600

    feat: add new tools page and popular software component

--- src/modules/new-tools/new-tools.page.tsx
+++ src/modules/new-tools/new-tools.page.tsx
@@ -1,10 +1,12 @@
 import { ToolBanner } from "./tool-banner.component";
 import { SoftwareCard } from "./software-card.component";
+import { PopularSoftware } from "./popular-software.component";
 export const NewToolsPage = () => {
   return (
     <div>
       <ToolBanner />
       <SoftwareCard />
+      <PopularSoftware />
     </div>
   );
 };
---
+++ src/modules/new-tools/popular-software.component.tsx
@@ -0,0 +1,339 @@
+import { Container } from "@/components/shared/container.component";
+import { SquareArrowOutUpRight, Star } from "lucide-react";
+
+export const PopularSoftware = () => {
+  return (
+    <section>
+      <Container>
+        <div className="space-y-2 px-10 lg:px-0">
+          <h2 className="text-2xl md:text-3xl font-bold">Popular Software</h2>
+          <p className="text-xl md:text-2xl">
+            Dive into our curated collection of groundbreaking AI and SaaS tools
+            designed to revolutionize your workflow and boost productivity.
+          </p>
+        </div>
+
+        <div className="flex flex-col lg:flex-row lg:justify-between items-center mt-12 space-y-7 lg:space-y-0">
+          <div className="flex items-center gap-5">
+            <div className="lg:w-[140px] lg:h-[250px] flex items-center px-3">
+              <img src="/home/software-1.png" alt="" />
+            </div>
+            <div className="max-w-[350px] space-y-2">
+              <p className="text-[16px] pr-3 md:pr-0">
+                <span className="font-bold">1. Cloudways</span> — Cloudways is
+                simple to use and provides flexible plans. It's one of the best
+                hosting platforms for…{" "}
+                <span className="border-b border-dashed cursor-pointer hover:underline underline-offset-4">
+                  Cloudways Review
+                </span>
+              </p>
+              <div className="flex items-center gap-2">
+                <div className="flex gap-0.5">
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                </div>
+                <span className="font-bold">93%</span>
+              </div>
+            </div>
+          </div>
+          <div className="max-w-[350px]">
+            <div className="flex justify-between items-center gap-20 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">
+                Starting price
+              </h5>
+              <p className="text-sm font-bold ">$10.00/mo</p>
+            </div>
+            <div className="flex justify-between items-center gap-5 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">Pricing model</h5>
+              <p className="text-sm font-bold ">Per License</p>
+            </div>
+            <div className="flex justify-between items-center gap-5 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">Free Trial</h5>
+              <p className="text-sm font-bold ">Yes</p>
+            </div>
+            <div className="flex justify-between items-center gap-5 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">Free Version</h5>
+              <p className="text-sm font-bold ">No</p>
+            </div>
+          </div>
+          <div className="">
+            <button className="flex gap-2 items-center py-4 px-20 border bg-gray-50 text-black hover:bg-gray-100 rounded-[10px] ">
+              <p className="font-bold text-[18px]">visit</p>
+              <SquareArrowOutUpRight className="w-5 h-5 stroke-3" />
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col lg:flex-row lg:justify-between items-center mt-12 space-y-7 lg:space-y-0">
+          <div className="flex items-center gap-5">
+            <div className="lg:w-[140px] lg:h-[250px] flex items-center px-3">
+              <img src="/home/software-1.png" alt="" />
+            </div>
+            <div className="max-w-[350px] space-y-2">
+              <p className="text-[16px] pr-3 md:pr-0 ">
+                <span className="font-bold">1. Cloudways</span> — Cloudways is
+                simple to use and provides flexible plans. It's one of the best
+                hosting platforms for…{" "}
+                <span className="border-b border-dashed cursor-pointer hover:underline underline-offset-4">
+                  Cloudways Review
+                </span>
+              </p>
+              <div className="flex items-center gap-2">
+                <div className="flex gap-0.5">
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                </div>
+                <span className="font-bold">93%</span>
+              </div>
+            </div>
+          </div>
+          <div className="max-w-[350px]">
+            <div className="flex justify-between items-center gap-20 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">
+                Starting price
+              </h5>
+              <p className="text-sm font-bold ">$10.00/mo</p>
+            </div>
+            <div className="flex justify-between items-center gap-5 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">Pricing model</h5>
+              <p className="text-sm font-bold ">Per License</p>
+            </div>
+            <div className="flex justify-between items-center gap-5 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">Free Trial</h5>
+              <p className="text-sm font-bold ">Yes</p>
+            </div>
+            <div className="flex justify-between items-center gap-5 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">Free Version</h5>
+              <p className="text-sm font-bold ">No</p>
+            </div>
+          </div>
+          <div className="">
+            <button className="flex gap-2 items-center py-4 px-20 border bg-gray-50 text-black hover:bg-gray-100 rounded-[10px] ">
+              <p className="font-bold text-[18px]">visit</p>
+              <SquareArrowOutUpRight className="w-5 h-5 stroke-3" />
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col lg:flex-row lg:justify-between items-center mt-12 space-y-7 lg:space-y-0">
+          <div className="flex items-center gap-5">
+            <div className="lg:w-[140px] lg:h-[250px] flex items-center px-3">
+              <img src="/home/software-1.png" alt="" />
+            </div>
+            <div className="max-w-[350px] space-y-2">
+              <p className="text-[16px] pr-3 md:pr-0">
+                <span className="font-bold">1. Cloudways</span> — Cloudways is
+                simple to use and provides flexible plans. It's one of the best
+                hosting platforms for…{" "}
+                <span className="border-b border-dashed cursor-pointer hover:underline underline-offset-4">
+                  Cloudways Review
+                </span>
+              </p>
+              <div className="flex items-center gap-2">
+                <div className="flex gap-0.5">
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                </div>
+                <span className="font-bold">93%</span>
+              </div>
+            </div>
+          </div>
+          <div className="max-w-[350px]">
+            <div className="flex justify-between items-center gap-20 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">
+                Starting price
+              </h5>
+              <p className="text-sm font-bold ">$10.00/mo</p>
+            </div>
+            <div className="flex justify-between items-center gap-5 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">Pricing model</h5>
+              <p className="text-sm font-bold ">Per License</p>
+            </div>
+            <div className="flex justify-between items-center gap-5 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">Free Trial</h5>
+              <p className="text-sm font-bold ">Yes</p>
+            </div>
+            <div className="flex justify-between items-center gap-5 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">Free Version</h5>
+              <p className="text-sm font-bold ">No</p>
+            </div>
+          </div>
+          <div className="">
+            <button className="flex gap-2 items-center py-4 px-20 border bg-gray-50 text-black hover:bg-gray-100 rounded-[10px] ">
+              <p className="font-bold text-[18px]">visit</p>
+              <SquareArrowOutUpRight className="w-5 h-5 stroke-3" />
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col lg:flex-row lg:justify-between items-center mt-12 space-y-7 lg:space-y-0">
+          <div className="flex items-center gap-5">
+            <div className="lg:w-[140px] lg:h-[250px] flex items-center px-3">
+              <img src="/home/software-1.png" alt="" />
+            </div>
+            <div className="max-w-[350px] space-y-2">
+              <p className="text-[16px] pr-3 md:pr-0">
+                <span className="font-bold">1. Cloudways</span> — Cloudways is
+                simple to use and provides flexible plans. It's one of the best
+                hosting platforms for…{" "}
+                <span className="border-b border-dashed cursor-pointer hover:underline underline-offset-4">
+                  Cloudways Review
+                </span>
+              </p>
+              <div className="flex items-center gap-2">
+                <div className="flex gap-0.5">
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                </div>
+                <span className="font-bold">93%</span>
+              </div>
+            </div>
+          </div>
+          <div className="max-w-[350px]">
+            <div className="flex justify-between items-center gap-20 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">
+                Starting price
+              </h5>
+              <p className="text-sm font-bold ">$10.00/mo</p>
+            </div>
+            <div className="flex justify-between items-center gap-5 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">Pricing model</h5>
+              <p className="text-sm font-bold ">Per License</p>
+            </div>
+            <div className="flex justify-between items-center gap-5 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">Free Trial</h5>
+              <p className="text-sm font-bold ">Yes</p>
+            </div>
+            <div className="flex justify-between items-center gap-5 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">Free Version</h5>
+              <p className="text-sm font-bold ">No</p>
+            </div>
+          </div>
+          <div className="">
+            <button className="flex gap-2 items-center py-4 px-20 border bg-gray-50 text-black hover:bg-gray-100 rounded-[10px] ">
+              <p className="font-bold text-[18px]">visit</p>
+              <SquareArrowOutUpRight className="w-5 h-5 stroke-3" />
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col lg:flex-row lg:justify-between items-center mt-12 space-y-7 lg:space-y-0">
+          <div className="flex items-center gap-5">
+            <div className="lg:w-[140px] lg:h-[250px] flex items-center px-3">
+              <img src="/home/software-1.png" alt="" />
+            </div>
+            <div className="max-w-[350px] space-y-2">
+              <p className="text-[16px] pr-3 md:pr-0">
+                <span className="font-bold">1. Cloudways</span> — Cloudways is
+                simple to use and provides flexible plans. It's one of the best
+                hosting platforms for…{" "}
+                <span className="border-b border-dashed cursor-pointer hover:underline underline-offset-4">
+                  Cloudways Review
+                </span>
+              </p>
+              <div className="flex items-center gap-2">
+                <div className="flex gap-0.5">
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                </div>
+                <span className="font-bold">93%</span>
+              </div>
+            </div>
+          </div>
+          <div className="max-w-[350px]">
+            <div className="flex justify-between items-center gap-20 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">
+                Starting price
+              </h5>
+              <p className="text-sm font-bold ">$10.00/mo</p>
+            </div>
+            <div className="flex justify-between items-center gap-5 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">Pricing model</h5>
+              <p className="text-sm font-bold ">Per License</p>
+            </div>
+            <div className="flex justify-between items-center gap-5 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">Free Trial</h5>
+              <p className="text-sm font-bold ">Yes</p>
+            </div>
+            <div className="flex justify-between items-center gap-5 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">Free Version</h5>
+              <p className="text-sm font-bold ">No</p>
+            </div>
+          </div>
+          <div className="">
+            <button className="flex gap-2 items-center py-4 px-20 border bg-gray-50 text-black hover:bg-gray-100 rounded-[10px] ">
+              <p className="font-bold text-[18px]">visit</p>
+              <SquareArrowOutUpRight className="w-5 h-5 stroke-3" />
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col lg:flex-row lg:justify-between items-center mt-12 space-y-7 lg:space-y-0">
+          <div className="flex items-center gap-5">
+            <div className="lg:w-[140px] lg:h-[250px] flex items-center px-3">
+              <img src="/home/software-1.png" alt="" />
+            </div>
+            <div className="max-w-[350px] space-y-2">
+              <p className="text-[16px] pr-3 md:pr-0">
+                <span className="font-bold">1. Cloudways</span> — Cloudways is
+                simple to use and provides flexible plans. It's one of the best
+                hosting platforms for…{" "}
+                <span className="border-b border-dashed cursor-pointer hover:underline underline-offset-4">
+                  Cloudways Review
+                </span>
+              </p>
+              <div className="flex items-center gap-2">
+                <div className="flex gap-0.5">
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                  <Star className="fill-yellow-500 color-yellow-500 stroke-yellow-500 w-5 h-5" />
+                </div>
+                <span className="font-bold">93%</span>
+              </div>
+            </div>
+          </div>
+          <div className="max-w-[350px]">
+            <div className="flex justify-between items-center gap-20 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">
+                Starting price
+              </h5>
+              <p className="text-sm font-bold ">$10.00/mo</p>
+            </div>
+            <div className="flex justify-between items-center gap-5 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">Pricing model</h5>
+              <p className="text-sm font-bold ">Per License</p>
+            </div>
+            <div className="flex justify-between items-center gap-5 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">Free Trial</h5>
+              <p className="text-sm font-bold ">Yes</p>
+            </div>
+            <div className="flex justify-between items-center gap-5 border-b border-gray-200 py-2">
+              <h5 className="text-gray-500 uppercase text-sm">Free Version</h5>
+              <p className="text-sm font-bold ">No</p>
+            </div>
+          </div>
+          <div className="">
+            <button className="flex gap-2 items-center py-4 px-20 border bg-gray-50 text-black hover:bg-gray-100 rounded-[10px] ">
+              <p className="font-bold text-[18px]">visit</p>
+              <SquareArrowOutUpRight className="w-5 h-5 stroke-3" />
+            </button>
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+PopularSoftware.displayName = "PopularSoftware";
