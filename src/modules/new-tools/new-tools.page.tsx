2385697bc87db539ac2a6dab646acd386562955a
naziachowdhurydev <naziachowdhurydev@gmail.com>
2026-02-15 21:22:15 +0600

    feat: Implement the new tools page, including components for software cards, popular listings, and FAQs.

--- src/modules/new-tools/new-tools.page.tsx
+++ src/modules/new-tools/new-tools.page.tsx
@@ -1,12 +1,15 @@
 import { ToolBanner } from "./tool-banner.component";
 import { SoftwareCard } from "./software-card.component";
 import { PopularSoftware } from "./popular-software.component";
+import { ToolFaqs } from "./tool-faqs.component";
+
 export const NewToolsPage = () => {
   return (
     <div>
       <ToolBanner />
       <SoftwareCard />
       <PopularSoftware />
+      <ToolFaqs />
     </div>
   );
 };
